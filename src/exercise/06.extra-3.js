c158fb43c46fd85676a60fff4e1979709f244be5
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-01-07 08:53:27 +0000

    finish ex-06.extra-3

---
+++ src/exercise/06.extra-3.js
@@ -0,0 +1,41 @@
+// Basic Forms
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+  const [username, setUsername] = React.useState('')
+
+  const handleChange = event => {
+    const {value} = event.target
+    setUsername(value.toLowerCase())
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    const username = event.target.elements.usernameInput.value
+    onSubmitUsername(username)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="usernameInput">Username:</label>
+        <input
+          id="usernameInput"
+          type="text"
+          onChange={handleChange}
+          value={username}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App
