d3c93f63e713ecf2c3e165389cac197de4504c16
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-01-07 07:53:20 +0000

    finish ex-05.extra-1

---
+++ src/exercise/05.extra-1.js
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import '../box-styles.css'
+
+const Box = ({className, style, ...otherProps}) => (
+  <div
+    className={`box ${className}`}
+    style={{fontStyle: 'italic', ...style}}
+    {...otherProps}
+  />
+)
+
+function App() {
+  return (
+    <div>
+      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box className="box--large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
+    </div>
+  )
+}
+
+export default App
