02744b3ec4056d8100b07f8222d8b7820fbfed04
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-01-08 07:47:22 +0000

    finish ex-07

--- src/exercise/07.original.js
+++ src/exercise/07.original.js
@@ -31,7 +31,8 @@ function App() {
       </button>
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
         {items.map(item => (
-          <li key={item.id}>
+          // 🐨 add a key prop to the <li> below. Set it to item.id
+          <li>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
             <input id={`${item.id}-input`} defaultValue={item.value} />
